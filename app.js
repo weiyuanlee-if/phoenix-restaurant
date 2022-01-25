d7754f2be0bdc249d6f208f5e0f9b28d9209af29
Lixian <weiyuanleeif@gmail.com>
2022-01-24 21:36:25 -0500

    added logo made navbar responsive

---
+++ app.js
@@ -0,0 +1,37 @@
+//Hamburger Toggle Button
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => { 
+    navbarLinks.classList.toggle('active')
+})
+
+//slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("myFoodSlides");
+    var dots = document.getElementsByClassName("dot");
+    if( n > slides.length) 
+    { 
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for(i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
